71a4deedc782b22235b1f382914bacaacdb28015
Urvashi003 <urvashibullani003@gmail.com>
2024-03-30 22:11:56 +0530

    potd of gfg and leetcode

---
+++ 30.03.2024/gfg.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // Code here
+       while(root->left) {
+           root = root->left;
+           
+           
+       }
+       return root->data;
+    }
+};
